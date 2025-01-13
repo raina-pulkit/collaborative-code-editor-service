f79fc863dca0d748d007366469bb306e3ac14144
raina-pulkit <pulkitraina2608@gmail.com>
2025-01-13 20:00:05 +0530

    add Fastify support to NestJS application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isDev = process.env.NODE_ENV === 'dev';
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: isDev }),
+  );
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
