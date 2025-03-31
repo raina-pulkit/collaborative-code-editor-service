2733fd86fda768c810d9051415414527ab11d8f3
raina-pulkit <pulkitraina2608@gmail.com>
2025-03-31 15:48:30 +0530

    Refactor UpdateRoomDto to remove unused properties
    - Simplify UpdateRoomDto by removing ownerUuid, isPrivate, and invitedUsers fields
    - Retain only the PartialType inheritance from CreateRoomDto for cleaner code

--- src/modules/room/dto/update-room.dto.ts
+++ src/modules/room/dto/update-room.dto.ts
@@ -1,23 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsArray, IsBoolean, IsOptional, IsUUID } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
 import { CreateRoomDto } from './create-room.dto';
 
-export class UpdateRoomDto extends PartialType(CreateRoomDto) {
-  @IsUUID()
-  @IsOptional()
-  @ApiProperty({ description: `Room owner uuid`, nullable: true })
-  ownerUuid?: string;
-
-  @IsBoolean()
-  @IsOptional()
-  @ApiProperty({ description: `Room's privacy`, nullable: true })
-  isPrivate?: boolean;
-
-  @IsArray()
-  @ApiProperty({
-    description: 'Array of users invited to the room',
-    nullable: true,
-  })
-  @IsUUID(undefined, { each: true })
-  invitedUsers?: string[];
-}
+export class UpdateRoomDto extends PartialType(CreateRoomDto) {}
